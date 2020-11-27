f1233b506926c34c7202f9430a6d10f9a36496a4
xingzhi <xingzhi2@illinois.edu>
2020-11-27 17:59:05 -0600

    hh

--- config.sh
+++ config.sh
@@ -4,3 +4,6 @@ echo "module.exports = {
     mongoUrl: 'mongodb+srv://baduk:baduk@cluster0.319wl.mongodb.net/baduk_online?retryWrites=true&w=majority',
     JWT_SECRET: 'my secret'
 }" > $SHELL_FOLDER/Baduk-Online-Backend/env.js
+
+echo "const SERVER_URL = 'http://35.226.255.75:7777/'
+export {SERVER_URL}" > $SHELL_FOLDER/Baduk-Online-Frontend/src/config.js
