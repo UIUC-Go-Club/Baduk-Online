6a153af7810c8a008b5777b1251f12dadf589907
xingzhi <xingzhi2@illinois.edu>
2020-11-27 16:00:35 -0600

    ll

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ echo npm start --prefix $SHELL_FOLDER/Baduk-Online-Frontend
 echo npm start --prefix $SHELL_FOLDER/Baduk-Online-Backend
 #npm start --prefix $SHELL_FOLDER/Baduk-Online-Frontend &
 npm start --prefix $SHELL_FOLDER/Baduk-Online-Backend &
+wait
