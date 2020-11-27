f51bd55bf019ebd574a825caa47ddbe79e6bf8bf
xingzhi <xingzhi2@illinois.edu>
2020-11-27 16:10:48 -0600

    hh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 echo npm start --prefix $SHELL_FOLDER/Baduk-Online-Frontend
 echo npm start --prefix $SHELL_FOLDER/Baduk-Online-Backend
-#npm start --prefix $SHELL_FOLDER/Baduk-Online-Frontend &
-npm start --prefix $SHELL_FOLDER/Baduk-Online-Backend &
+npm start --prefix $SHELL_FOLDER/Baduk-Online-Frontend &
+#npm start --prefix $SHELL_FOLDER/Baduk-Online-Backend &
 wait
